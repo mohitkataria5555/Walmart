cc60509fe982e7287430b7cf237330c4261225a4
mohitkataria5555 <mohitkataria5555@gmail.com>
2023-09-16 22:00:40 +0530

    Third commit

--- src/main/java/com/project/walmart/service/CategoryService.java
+++ src/main/java/com/project/walmart/service/CategoryService.java
@@ -1,9 +1,11 @@
 package com.project.walmart.service;
 
 
+import com.project.walmart.Exception.ResourceNotFoundException;
 import com.project.walmart.model.Category;
 import com.project.walmart.payload.CategoryDto;
 import com.project.walmart.repository.CategoryRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,13 @@ import java.util.List;
 public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     public CategoryDto createCategory(CategoryDto categoryDto){
-
-
-
-        return  null;
+        Category cat=this.modelMapper.map(categoryDto,Category.class);
+        Category save=this.categoryRepository.save(cat);
+        return  this.modelMapper.map(save,CategoryDto.class);
     }
 
     public List<CategoryDto> viewAll(){
@@ -28,35 +31,25 @@ public class CategoryService {
     }
 
     public CategoryDto viewCategoryById(int cid){
+        Category getById=this.categoryRepository.findById(cid).orElseThrow(()->new ResourceNotFoundException("Category id Not Found."));
 
-        return null;
+        return this.modelMapper.map(getById,CategoryDto.class);
     }
 
     public void deleteCategory(int cid){
-
+        Category Cat=this.categoryRepository.findById(cid).orElseThrow(()->new ResourceNotFoundException("Category id Not Found."));
+        this.categoryRepository.delete(Cat);
 
     }
 
     public CategoryDto updateCategory(int cid, CategoryDto newc){
+        Category oldCat=this.categoryRepository.findById(cid).orElseThrow(()->new ResourceNotFoundException("Category id Not Found."));
+        oldCat.setCategory_id(newc.getCategory_id());
+        oldCat.setTitle(newc.getTitle());
 
-
-        return null;
+        return this.modelMapper.map(oldCat,CategoryDto.class);
 
     }
 
-
-    public Category toEntity(CategoryDto categoryDto){
-        Category c = new Category();
-        c.setTitle(categoryDto.getTitle());
-        return c;
-    }
-
-
-    public CategoryDto toDto(Category category){
-        CategoryDto categoryDto=new CategoryDto();
-        categoryDto.setTitle(category.getTitle());
-
-        return categoryDto;
-    }
 
 }
