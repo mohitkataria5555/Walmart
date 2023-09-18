01c1772e9810b157c7a93332db40f5bcf7765265
mohitkataria5555 <mohitkataria5555@gmail.com>
2023-09-18 21:03:38 +0530

    user api created

--- src/main/java/com/project/walmart/controller/UserController.java
+++ src/main/java/com/project/walmart/controller/UserController.java
@@ -1,16 +1,16 @@
 package com.project.walmart.controller;
 
+import com.project.walmart.model.User;
 import com.project.walmart.payload.UserDto;
 import com.project.walmart.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -24,6 +24,31 @@ public class UserController {
         userDto.setDate(dated);
         UserDto createuser=this.userService.createUser(userDto);
         return new ResponseEntity<UserDto>(createuser, HttpStatus.CREATED);
+    }
+    @GetMapping("/findById/{userId}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable int userId){
+        Optional<User> user = this.userService.getUserById(userId);
+        return new ResponseEntity<Optional<User>>(user,HttpStatus.FOUND);
+
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteTheUser(@PathVariable int userId){
+       userService.deleteUser(userId);
+       return new ResponseEntity<String>("User Deleted",HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/view")
+    public ResponseEntity<List<UserDto>> viewAllUsers(){
+        List<UserDto> userDto= this.userService.viewAll();
+        return new ResponseEntity<List<UserDto>>(userDto,HttpStatus.ACCEPTED);
+    }
+    @PutMapping("/update/{userId}")
+    public ResponseEntity<UserDto> update(@PathVariable int userId,@RequestBody UserDto userDto ){
+        Date dated= new Date();
+        userDto.setDate(dated);
+        UserDto updateUser=this.userService.updateUserInfo(userId,userDto);
+        return new ResponseEntity<UserDto>(updateUser,HttpStatus.ACCEPTED);
     }
 
 
--- src/main/java/com/project/walmart/service/UserService.java
+++ src/main/java/com/project/walmart/service/UserService.java
@@ -1,11 +1,18 @@
 package com.project.walmart.service;
 
+import com.project.walmart.Exception.ResourceNotFoundException;
+import com.project.walmart.model.Category;
 import com.project.walmart.model.User;
+import com.project.walmart.payload.CategoryDto;
 import com.project.walmart.payload.UserDto;
 import com.project.walmart.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -23,6 +30,39 @@ public class UserService {
         return saveUserDto;
     }
 
+    public Optional<User> getUserById(int userId){
+        Optional<User> user= Optional.ofNullable(this.userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User id not found")));
+        return user;
+    }
+
+    public  void  deleteUser(int userId){
+        User user= this.userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User id not found"));
+        userRepository.delete(user);
+    }
+
+    public List<UserDto> viewAll(){
+        List<User> findall=this.userRepository.findAll();
+        List<UserDto> allDto =findall.stream().map(user -> this.modelMapper.map(user,UserDto.class)).collect(Collectors.toList());
+
+        return allDto;
+    }
+
+    public UserDto updateUserInfo(int userId, UserDto userDto){
+        User olduser=this.userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User id not found"));
+        olduser.setName(userDto.getName());
+        olduser.setGender(userDto.getGender());
+        olduser.setAbout(userDto.getAbout());
+        olduser.setActive(userDto.isActive());
+        olduser.setAddress(userDto.getAddress());
+        olduser.setEmail(userDto.getEmail());
+        olduser.setPassword(userDto.getPassword());
+        olduser.setPhone(userDto.getPhone());
+        olduser.setDate(userDto.getDate());
+
+        User save= this.userRepository.save(olduser);
+
+        return this.modelMapper.map(save,UserDto.class);
+    }
 
 
 }
